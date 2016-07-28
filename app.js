e2142f1c5a8c24220f8a5494e1fa87f7dbede245
zhangtiantian10 <975571814@qq.com>
2016-07-28 23:20:12 +0800

    add app.get and .post

--- app.js
+++ app.js
@@ -1,51 +1,27 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict'
 
-var routes = require('./routes/index');
+const express = require('express');
+const AnswerGenerator = require('./controllers/answer-generator');
+const CompareNumber = require('./controllers/compare-number');
+const JudeInput = require('./controllers/jude-input');
+const bodyParser = require('body-parser');
+const router = express();
 
-var app = express();
+router.use(bodyParser.json());
 
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
-
-app.use('/', routes);
-
-/// catch 404 and forwarding to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
+router.get('/answer', (req, res) => {
+    res.send(AnswerGenerator.getAnswer());
 });
 
-/// error handlers
+router.post('/compare', (req, res) => {
 
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
+    if(!JudeInput.jude(req.body.input)) {
+        res.send('Cannot input duplicate numbers!');
+    }
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
+    res.send(CompareNumber.output(req.body.input, req.body.answer));
 });
 
-
-module.exports = app;
+router.listen(3000, () => {
+    console.log('server start');
+})
